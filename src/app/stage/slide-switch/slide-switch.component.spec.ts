cf0fa0ab82bcabca7c53e42a4b847b979c3221f2
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-05-02 00:03:36 +0200

    Add switch slider

---
+++ src/app/stage/slide-switch/slide-switch.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SlideSwitchComponent } from './slide-switch.component';
+
+describe('SlideSwitchComponent', () => {
+  let component: SlideSwitchComponent;
+  let fixture: ComponentFixture<SlideSwitchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SlideSwitchComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlideSwitchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/stage/slide-switch/slide-switch.component.ts
@@ -0,0 +1,82 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  HostBinding,
+  Output,
+  EventEmitter,
+  ViewChildren,
+  ElementRef,
+  HostListener,
+  QueryList } from '@angular/core';
+
+export interface SwitchOption {
+  label: string;
+  value: string;
+}
+
+@Component({
+  selector: 'jsr-slide-switch',
+  templateUrl: './slide-switch.component.html',
+  styleUrls: ['./slide-switch.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SlideSwitchComponent {
+  @HostBinding('attr.tabindex')
+  tabIndex = '0';
+
+  @Input()
+  options: SwitchOption[];
+
+  @Input()
+  group: string;
+
+  @Input()
+  value: string;
+
+  @Output()
+  valueChanged = new EventEmitter<string>();
+
+  @ViewChildren('control')
+  controls: QueryList<ElementRef>;
+
+  @HostListener('focus')
+  onFocus() {
+    if (!this.controls.length) {
+      return;
+    }
+
+    const index = this.selectedIndex();
+    this.controls.toArray()[index].nativeElement.focus();
+  }
+
+  @HostListener('keyup.arrowup')
+  selectPrev() {
+    const currentIndex = this.selectedIndex();
+
+    if (currentIndex > 0) {
+      const value = this.options[currentIndex - 1].value;
+      this.valueChanged.emit(value);
+    }
+  }
+
+  @HostListener('keyup.arrowdown')
+  selectNext() {
+    const currentIndex = this.selectedIndex();
+
+    if (currentIndex < this.options.length - 1) {
+      const value = this.options[currentIndex + 1].value;
+      this.valueChanged.emit(value);
+    }
+  }
+
+  onValueChange(event) {
+    const value = event.target.value;
+    this.valueChanged.emit(value);
+  }
+
+  private selectedIndex(): number {
+    return this.value ? this.options
+      .findIndex((option) => option.value === this.value) : 0;
+  }
+}
