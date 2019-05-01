819d4627f6071e594ca2114c724fdf95a93db05a
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-05-01 16:17:55 +0200

    Fix removal of effect

--- src/app/audio/audio-context-manager.service.ts
+++ src/app/audio/audio-context-manager.service.ts
@@ -73,7 +73,7 @@ export class AudioContextManager {
 
   removeEffect(effect: Effect) {
     this.disconnectAll();
-    this.effects = this.effects.filter(eff => eff === effect);
+    this.effects = this.effects.filter(eff => eff !== effect);
     this.connectInOrder();
   }
 
