7fbc4b62d6bc7ce010dd18fc0ae9d4094c36336a
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-05-09 22:09:27 -0400

    change oversample

--- src/app/audio/effects/distortion.ts
+++ src/app/audio/effects/distortion.ts
@@ -58,7 +58,7 @@ export class Distortion extends Effect {
   ) {
     super(context, model);
 
-    this.waveSharper = new WaveShaperNode(context, {oversample: '4x'});
+    this.waveSharper = new WaveShaperNode(context, {oversample: '2x'});
     this.toneNode = Tone(context);
     this.levelNode = context.createGain();
 
