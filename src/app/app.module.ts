b11fc1e0f8f102c5252639159c0949234f0067eb
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-05-31 19:34:02 +0200

    Updat angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
