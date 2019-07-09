6fe1edbfe6483f6706fac9dbcf1ee73fc3786278
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-07-09 23:09:40 +0200

    Add dB converters

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,53 +1,59 @@
 import { EffectNode } from '@audio/node.interface';
 
-export interface FadeInOptions {
-  target: GainNode;
-  dest?: AudioNode;
-  time: number;
-  value?: number;
-}
-
-export function clamp(min, max, value) {
+/**
+ * Clamps value to provided min and max - min <= value <= max.
+ */
+export function clamp(min: number, max: number, value: number): number {
   return Math.min(Math.max(value, min), max);
 }
 
+/**
+ * Connects effect nodes in order.
+ */
 export function connectNodes(nodes: EffectNode[]) {
   for (let i = nodes.length - 1; i > 0; --i) {
     nodes[i - 1].connect(nodes[i]);
   }
 }
 
-export function mapToMinMax(value, min, max) {
+/**
+ * Maps value to min max range.
+ */
+export function mapToMinMax(value, min, max): number {
   return value * (max - min) + min;
 }
 
-export function percentFromMinMax(value, min, max) {
+/**
+ * Gets value percentage from min max range.
+ */
+export function percentFromMinMax(value, min, max): number {
   return ((value - min)) / (max - min);
 }
 
-export function expScale(value) {
+/**
+ * Rescales value exponentially.
+ */
+export function expScale(value): number {
   return Math.pow(Math.abs(value), 2);
 }
 
-export function gainFadeInConnect(options: FadeInOptions) {
-  const value = options.value || options.target.gain.value;
-  options.target.gain.value = 0;
-
-  if (options.dest) {
-    options.target.connect(options.dest);
-  }
-
-  options.target.gain.setTargetAtTime(value, options.time, 0.01);
-}
-
-export function toMs(value: number) {
+/**
+ * Converts seconds to milliseconds.
+ */
+export function toMs(value: number): number {
   return value > 0 ? value / 1000 : 0;
 }
 
+/**
+ * Fast deep copy of a simple object.
+ */
 export function deepCopy(obj: any): any {
   return JSON.parse(JSON.stringify(obj));
 }
 
+/**
+ * Equal power cross-fade.
+ */
 export function equalCrossFade(value: number): [number, number] {
   const first = value === 1 ? 0 : Math.cos(value * 0.5 * Math.PI);
   const last = value === 0 ? 0 : Math.cos((1 - value) * 0.5 * Math.PI);
@@ -55,10 +61,16 @@ export function equalCrossFade(value: number): [number, number] {
   return [first, last];
 }
 
-export function calculateCenterFrequency(range: [number, number]) {
+/**
+ * Calculates biquad filter center frequency for a given frequencies range.
+ */
+export function calculateCenterFrequency(range: [number, number]): number {
   return Math.sqrt(range[0] *  range[1]);
 }
 
+/**
+ * Calculates center frequency and Q (quality) parameter for bandpass filter.
+ */
 export function calculateBandpass(range: [number, number]): {fc: number; q: number;} {
   const fc = calculateCenterFrequency(range);
   const q = fc / (range[1] - range[0]);
@@ -66,3 +78,32 @@ export function calculateBandpass(range: [number, number]): {fc: number; q: numb
   return {fc, q};
 }
 
+/**
+ * Computation fast Math.tanh approximation.
+ */
+export function fastTanh(x: number): number {
+  // Limit out of range data.
+  if (x < -3) {
+    x = -1;
+  } else if (x > 3) {
+    x = 1;
+  } else {
+    x = x * (27 + x * x) / (27 + 9 * x * x);
+  }
+
+  return x;
+}
+
+/**
+ * Converts gain value to dB.
+ */
+export function gainToDB(value: number): number {
+  return 20 * Math.log10(value);
+}
+
+/**
+ * Converts dB value to gain.
+ */
+export function dBToGain(value: number): number {
+  return Math.pow(10, (value / 20));
+}
