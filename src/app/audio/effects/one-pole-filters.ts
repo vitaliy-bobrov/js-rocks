672284d6878fc6b568206c3f6f0de5d0908e146d
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-07-09 23:10:08 +0200

    Add one-pole filters

---
+++ src/app/audio/effects/one-pole-filters.ts
@@ -0,0 +1,33 @@
+// Based on https://beis.de/Elektronik/Filter/AnaDigFilt/1stOrderDigFilt.html
+
+export interface IIRCoefficients {
+  feedForward: number[];
+  feedback: number[];
+}
+
+/**
+ * Creates one-pole low pass IIR filter coefficients for cut-off frequency.
+ */
+export function onePoleLowpass(frequency: number, sampleRate: number): IIRCoefficients {
+  const t = 1 / (2 * Math.PI * frequency);
+  const a0 = 1 / (t * sampleRate);
+  const b1 = a0 - 1;
+
+  return {
+    feedForward: [a0, 0],
+    feedback: [1, b1]
+  };
+}
+
+/**
+ * Creates one-pole high pass IIR filter coefficients for cut-off frequency.
+ */
+export function onePoleHighpass(frequency: number, sampleRate: number): IIRCoefficients {
+  const t = 1 / (2 * Math.PI * frequency);
+  const b1 = 1 / (t * sampleRate) - 1;
+
+  return {
+    feedForward: [1, -1],
+    feedback: [1, b1]
+  };
+}
