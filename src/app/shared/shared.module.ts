3d550d94829796d0c72a7c8370cf5318d12bf0d9
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-10-16 20:54:53 +0200

    Move shared pipes to separate folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PortalModule } from '@angular/cdk/portal';
 import { PageActionsComponent } from './page-actions/page-actions.component';
-import { ValueLabelPipe } from './value-label.pipe';
-import { StringPipe } from './string.pipe';
+import { ValueLabelPipe } from './pipes/value-label.pipe';
+import { StringPipe } from './pipes/string.pipe';
 
 @NgModule({
-  declarations: [
-    PageActionsComponent,
-    ValueLabelPipe,
-    StringPipe
-  ],
-  imports: [
-    PortalModule
-  ],
-  exports: [
-    PageActionsComponent,
-    PortalModule,
-    ValueLabelPipe,
-    StringPipe
-  ]
+  declarations: [PageActionsComponent, ValueLabelPipe, StringPipe],
+  imports: [PortalModule],
+  exports: [PageActionsComponent, PortalModule, ValueLabelPipe, StringPipe]
 })
 export class SharedModule {}
