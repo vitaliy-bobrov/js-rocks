9c04ecaf9f7125737e168de403a1a5ea32e35cc7
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-11-05 22:13:15 +0100

    Update jest to v8

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -14,17 +14,14 @@ const mock = () => {
 Object.defineProperty(window, 'localStorage', { value: mock() });
 Object.defineProperty(window, 'sessionStorage', { value: mock() });
 Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ['-webkit-appearance'],
+  value: () => ['-webkit-appearance']
 });
 
 Object.defineProperty(document.body.style, 'transform', {
   value: () => {
     return {
       enumerable: true,
-      configurable: true,
+      configurable: true
     };
-  },
+  }
 });
-
-/* output shorter and more meaningful Zone error stack traces */
-// Error.stackTraceLimit = 2;
