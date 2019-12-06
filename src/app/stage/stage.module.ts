0f4d30ea136d89ab18b7ed6112cef170fbeb995f
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2019-12-06 16:36:38 +0100

    Correct binding for matBadge

--- src/app/stage/stage.module.ts
+++ src/app/stage/stage.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatBadgeModule } from '@angular/material';
 import {
   MatSnackBarModule,
   MAT_SNACK_BAR_DEFAULT_OPTIONS
