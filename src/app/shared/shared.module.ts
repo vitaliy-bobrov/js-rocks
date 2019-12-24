e593d22354c3d0959847cc57394b6299f4cc4a8b
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-12-24 16:30:30 +0100

    Remove string pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { PortalModule } from '@angular/cdk/portal';
 import { PageActionsComponent } from './page-actions/page-actions.component';
 import { ValueLabelPipe } from './pipes/value-label.pipe';
-import { StringPipe } from './pipes/string.pipe';
 import { BitMaskPipe } from './pipes/bit-mask.pipe';
 
 @NgModule({
-  declarations: [PageActionsComponent, ValueLabelPipe, StringPipe, BitMaskPipe],
+  declarations: [PageActionsComponent, ValueLabelPipe, BitMaskPipe],
   imports: [PortalModule],
-  exports: [
-    BitMaskPipe,
-    PageActionsComponent,
-    PortalModule,
-    StringPipe,
-    ValueLabelPipe
-  ]
+  exports: [BitMaskPipe, PageActionsComponent, PortalModule, ValueLabelPipe]
 })
 export class SharedModule {}
