700a5ceb52c6b6d62fe55e531df482093c239c80
Vitaliy Bobrov <bobrov1989@gmail.com>
2020-01-03 00:00:13 +0100

    Remove pedalboard directive

--- src/app/stage/pedalboard/pedalboard.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[jsrPedalBoard]'
-})
-export class PedalBoardDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {}
-}
