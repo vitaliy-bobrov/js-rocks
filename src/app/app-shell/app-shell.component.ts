3445e8daca09951bbff47169166c71893806226e
Vitaliy Bobrov <bobrov1989@gmail.com>
2020-02-02 17:53:12 +0100

    feat(app-shell): Create application shell.

---
+++ src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'jsr-app-shell',
+  templateUrl: './app-shell.component.html',
+  styleUrls: ['./app-shell.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppShellComponent {}
